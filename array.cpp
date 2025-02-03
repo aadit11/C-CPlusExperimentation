7ecb4c48960f2494392b70b8a0630dd5c01b4f2c
aadit11 <aadit.kshirsagar@gmail.com>
2025-02-03 08:47:22 +0530

    Added a file

---
+++ array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+using namespace std;
+
+int main() 
+{
+	
+int n[5];
+cout<<"Enter 5 numbers: ";
+
+for (int i = 0; i < 5;i++) 
+{
+	
+   cin>>n[i];
+   cout<<"The numbers stored in an array are " << n[i];
+}
+
+
+
+ return 0;
+}
